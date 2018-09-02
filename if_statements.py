8993f968d04b1d42836f3e2106e27e9a9ee633c4
Michael Mallowe <mmallowe@gmail.com>
2018-09-01 23:55:55 -0400

    comparrison opperators

--- if_statements.py
+++ if_statements.py
@@ -14,3 +14,15 @@ elif is_alien and not is_tall:
 else:
     print("Sup not-dude")
 
+
+# return largest number
+def max_num(num1, num2, num3):
+    if num1 <= num2 and num1 != num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(0.00, -1, -34))
