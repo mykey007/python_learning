9ff2862053c5a48dca78c0629524131bbca93b16
Michael Mallowe <mmallowe@gmail.com>
2018-09-01 21:51:41 -0400

    first tutorial

---
+++ app.py
@@ -0,0 +1,26 @@
+character_name = "dink"
+character_species = "cat"
+character_age = "14"
+
+print("   /\\")
+print("  /  \\")
+print(" /    \\")
+print("/______\\")
+#print(len(character_age))
+#print(89 * 4322234)
+#name = input("Type your name!")
+#print("Hi" + name)
+
+#num1 = input("Enter a number:")
+#num2 = input("Enter another number:")
+#result = float(num1) + float(num2)
+#print(result)
+
+color = input("Enter a number: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+print("How about you???")
