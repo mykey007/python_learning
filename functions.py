162d2ac9b921d48bb34270ab0eb86a5fad773503
Michael Mallowe <mmallowe@gmail.com>
2018-09-01 23:25:20 -0400

    passing parameters into a function

---
+++ functions.py
@@ -0,0 +1,10 @@
+# say hi to the user
+
+def say_hi(name, language, animal):
+    print("Hi " + name + ". You speak " + language + ". You were born in the year of the " + animal.lower() + ".")
+
+# read top to bottom
+print("***** First *****")
+say_hi("Rie", "Japanese", "Rat")
+print("***** Last *****")
+
