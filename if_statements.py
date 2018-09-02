a84dab51ee09a769adf6f93f8c9eb96565fd6b45
Michael Mallowe <mmallowe@gmail.com>
2018-09-01 23:46:08 -0400

    booleans

---
+++ if_statements.py
@@ -0,0 +1,16 @@
+# if statements
+
+is_male = True
+is_tall = False
+is_alien = True
+
+if is_male and is_tall: # use or to get the first condition
+
+    print("Sup tall dude")
+
+elif is_alien and not is_tall:
+    print("Nanu Nanu Señor Alien")
+
+else:
+    print("Sup not-dude")
+
